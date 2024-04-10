0d99f1b76bef5b7aa5802c4dcae10b44257a9019
Andrew Roide <andrew.roide@hotmail.com>
2024-04-10 02:02:51 +0000

    adds a new API endpoint using GET to the Express.js server that successfully retrieves dishes along with their corresponding ingredients from the database

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars -- Remove when used */
 import 'dotenv/config';
 import express from 'express';
+import { type } from 'node:os';
+import { nextTick } from 'node:process';
 import pg from 'pg';
 import {
   ClientError,
   defaultMiddleware,
   errorMiddleware,
 } from './lib/index.js';
+
+type Entry = {
+  dishId: number;
+  title: string;
+  ingredientId: number;
+  photoUrl: string;
+};
 
 const connectionString =
   process.env.DATABASE_URL ||
@@ -29,8 +38,24 @@ app.use(express.static(reactStaticDir));
 app.use(express.static(uploadsStaticDir));
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello, World!' });
+app.get('/api/dishes', async (req, res, next) => {
+  try {
+    const sql = `
+    select "dishes"."id" as "dishId",
+           "dishes"."title",
+           "dishes"."photoUrl",
+           "ingredients"."id" as "ingredientId",
+           "ingredients"."name"
+      from "dishes"
+      join "dishIngredients" on "dishes"."id" = "dishIngredients"."dishId"
+      join "ingredients" on "dishIngredients"."ingredientId" = "ingredients"."id"
+      order by "dishes"."id" desc;
+  `;
+    const result = await db.query(sql);
+    res.status(201).json(result.rows);
+  } catch (error) {
+    next(error);
+  }
 });
 
 /*
